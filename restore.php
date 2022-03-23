bed90fc3dc24ea6eb6ecd5ec6967a96ddae74a44
Jkm-Pc2 <jacqueskatsuva@gmail.com>
2022-03-23 01:37:50 +0100

    essai restore

--- restore.php
+++ restore.php
@@ -89,8 +89,6 @@
 
     <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/"> -->
 
-
-
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
